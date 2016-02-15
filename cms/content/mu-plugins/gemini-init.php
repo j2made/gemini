d0767cf4c710a97dad70e771e44cb0804dae54c7
Shaun Baer <sbaer@j2designpartnership.com>
2016-02-15 11:18:17 -0500

    Updated Bedrock Autoloader

--- cms/content/mu-plugins/gemini-init.php
+++ cms/content/mu-plugins/gemini-init.php
@@ -77,7 +77,10 @@ add_filter( 'pre_option_blog_public', '__return_false' );
  * @since  1.0.0
  */
 
-class Bedrock_Autoloader {
+
+if (!is_blog_installed()) { return; }
+
+class Autoloader {
   private static $cache; // Stores our plugin cache and site option.
   private static $auto_plugins; // Contains the autoloaded plugins (only when needed).
   private static $mu_plugins; // Contains the mu plugins (only when needed).
@@ -85,20 +88,15 @@ class Bedrock_Autoloader {
   private static $activated; // Newly activated plugins.
   private static $relative_path; // Relative path to the mu-plugins dir.
   private static $_single; // Let's make this a singleton.
-
   public function __construct() {
     if (isset(self::$_single)) { return; }
-
     self::$_single       = $this; // Singleton set.
     self::$relative_path = '/../' . basename(__DIR__); // Rel path set.
-
     if (is_admin()) {
       add_filter('show_advanced_plugins', array($this, 'showInAdmin'), 0, 2); // Admin only filter.
     }
-
     $this->loadPlugins();
   }
-
   /**
    * Run some checks then autoload our plugins.
    */
@@ -106,50 +104,38 @@ class Bedrock_Autoloader {
     $this->checkCache();
     $this->validatePlugins();
     $this->countPlugins();
-
     foreach (self::$cache['plugins'] as $plugin_file => $plugin_info) {
       include_once(WPMU_PLUGIN_DIR . '/' . $plugin_file);
     }
-
     $this->pluginHooks();
   }
-
   /**
    * Filter show_advanced_plugins to display the autoloaded plugins.
    */
   public function showInAdmin($bool, $type) {
     $screen = get_current_screen();
     $current = is_multisite() ? 'plugins-network' : 'plugins';
-
     if ($screen->{'base'} != $current || $type != 'mustuse' || !current_user_can('activate_plugins')) {
       return $bool;
     }
-
     $this->updateCache(); // May as well update the transient cache whilst here.
-
     self::$auto_plugins = array_map(function ($auto_plugin) {
       $auto_plugin['Name'] .= ' *';
       return $auto_plugin;
     }, self::$auto_plugins);
-
     $GLOBALS['plugins']['mustuse'] = array_unique(array_merge(self::$auto_plugins, self::$mu_plugins), SORT_REGULAR);
-
     return false; // Prevent WordPress overriding our work.
   }
-
   /**
    * This sets the cache or calls for an update
    */
   private function checkCache() {
     $cache = get_site_option('bedrock_autoloader');
-
     if ($cache === false) {
       return $this->updateCache();
     }
-
     self::$cache = $cache;
   }
-
   /**
    * Get the plugins and mu-plugins from the mu-plugin path and remove duplicates.
    * Check cache against current plugins for newly activated plugins.
@@ -157,17 +143,14 @@ class Bedrock_Autoloader {
    */
   private function updateCache() {
     require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-
     self::$auto_plugins = get_plugins(self::$relative_path);
-    self::$mu_plugins   = get_mu_plugins(self::$relative_path);
+    self::$mu_plugins   = get_mu_plugins();
     $plugins            = array_diff_key(self::$auto_plugins, self::$mu_plugins);
     $rebuild            = !is_array(self::$cache['plugins']);
     self::$activated    = ($rebuild) ? $plugins : array_diff_key($plugins, self::$cache['plugins']);
     self::$cache        = array('plugins' => $plugins, 'count' => $this->countPlugins());
-
     update_site_option('bedrock_autoloader', self::$cache);
   }
-
   /**
    * This accounts for the plugin hooks that would run if the plugins were
    * loaded as usual. Plugins are removed by deletion, so there's no way
@@ -175,12 +158,10 @@ class Bedrock_Autoloader {
    */
   private function pluginHooks() {
     if (!is_array(self::$activated)) { return; }
-
     foreach (self::$activated as $plugin_file => $plugin_info) {
       do_action('activate_' . $plugin_file);
     }
   }
-
   /**
    * Check that the plugin file exists, if it doesn't update the cache.
    */
@@ -192,23 +173,18 @@ class Bedrock_Autoloader {
       }
     }
   }
-
   /**
    * Count our plugins (but only once) by counting the top level folders in the
    * mu-plugins dir. If it's more or less than last time, update the cache.
    */
   private function countPlugins() {
     if (isset(self::$count)) { return self::$count; }
-
     $count = count(glob(WPMU_PLUGIN_DIR . '/*/', GLOB_ONLYDIR | GLOB_NOSORT));
-
     if (!isset(self::$cache['count']) || $count != self::$cache['count']) {
       self::$count = $count;
       $this->updateCache();
     }
-
     return self::$count;
   }
 }
-
-new Bedrock_Autoloader();
+new Autoloader();
